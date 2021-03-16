25b323039bd385043ed9726153949128c49b6631
DCCXXVI726 <DCCXXVI726762@gmail.com>
2021-03-17 00:57:36 +0500

    2

--- fast.go
+++ fast.go
@@ -7,6 +7,9 @@ import (
 	"os"
 	"regexp"
 	"strings"
+        jlexer "github.com/mailru/easyjson/jlexer"
+        jwriter "github.com/mailru/easyjson/jwriter"
+
 	// "log"
 )
 
@@ -19,11 +22,14 @@ type User struct {
 
 const filePath1 string = "./data/users.txt"
 
+var pattern1 = regexp.MustCompile("Android")
+var pattern2 = regexp.MustCompile("MSIE")
 func FastSearch(out io.Writer) {
 	file, err := os.Open(filePath1)
 	if err != nil {
 		panic(err)
 	}
+	defer file.Close()
 
 	fileContents, err := ioutil.ReadAll(file)
 	if err != nil {
@@ -31,11 +37,11 @@ func FastSearch(out io.Writer) {
 	}
 
 	r := regexp.MustCompile("@")
-	seenBrowsers := []string{}
 	uniqueBrowsers := 0
 	foundUsers := ""
 
 	lines := strings.Split(string(fileContents), "\n")
+	seenBrowsers := make(map[string]bool)
 	i := -1
 	for _, line := range lines {
 		i++
@@ -48,36 +54,19 @@ func FastSearch(out io.Writer) {
 		isMSIE := false
 
 		for _, browser := range user.Browsers {
-			if ok, err := regexp.MatchString("Android", browser); ok && err == nil {
+			if ok := pattern1.MatchString(browser); ok {
 				isAndroid = true
-				notSeenBefore := true
-				for _, item := range seenBrowsers {
-					if item == browser {
-						notSeenBefore = false
-					}
-				}
-				if notSeenBefore {
-					// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
-					seenBrowsers = append(seenBrowsers, browser)
+				if ok, _ := seenBrowsers[browser]; !ok {
+					seenBrowsers[browser] = true
 					uniqueBrowsers++
 				}
 			}
-		}
-
-		for _, browser := range user.Browsers {
-			if ok, err := regexp.MatchString("MSIE", browser); ok && err == nil {
+			if ok := pattern2.MatchString(browser); ok {
 				isMSIE = true
-				notSeenBefore := true
-				for _, item := range seenBrowsers {
-					if item == browser {
-						notSeenBefore = false
-					}
-				}
-				if notSeenBefore {
-					// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
-					seenBrowsers = append(seenBrowsers, browser)
-					uniqueBrowsers++
-				}
+				if ok, _ := seenBrowsers[browser]; !ok {
+                                        seenBrowsers[browser] = true
+                                        uniqueBrowsers++
+                                }
 			}
 		}
 
@@ -91,5 +80,119 @@ func FastSearch(out io.Writer) {
 	}
 
 	fmt.Fprintln(out, "found users:\n"+foundUsers)
-	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
+	fmt.Fprintln(out, "Total unique browsers", uniqueBrowsers)
 }
+
+func easyjson9e1087fdDecodeTemp(in *jlexer.Lexer, out *User) {
+        isTopLevel := in.IsStart()
+        if in.IsNull() {
+                if isTopLevel {
+                        in.Consumed()
+                }
+                in.Skip()
+                return
+        }
+        in.Delim('{')
+        for !in.IsDelim('}') {
+                key := in.UnsafeFieldName(false)
+                in.WantColon()
+                if in.IsNull() {
+                        in.Skip()
+                        in.WantComma()
+                        continue
+                }
+                switch key {
+                case "browsers":
+                        if in.IsNull() {
+                                in.Skip()
+                                out.Browsers = nil
+                        } else {
+                                in.Delim('[')
+                                if out.Browsers == nil {
+                                        if !in.IsDelim(']') {
+                                                out.Browsers = make([]string, 0, 4)
+                                        } else {
+                                                out.Browsers = []string{}
+                                        }
+                                } else {
+                                        out.Browsers = (out.Browsers)[:0]
+                                }
+                                for !in.IsDelim(']') {
+                                        var v1 string
+                                        v1 = string(in.String())
+                                        out.Browsers = append(out.Browsers, v1)
+                                        in.WantComma()
+                                }
+                                in.Delim(']')
+                        }
+                case "email":
+                        out.Email = string(in.String())
+                case "name":
+                        out.Name = string(in.String())
+                default:
+                        in.SkipRecursive()
+                }
+                in.WantComma()
+        }
+       in.Delim('}')
+        if isTopLevel {
+                in.Consumed()
+        }
+}
+func easyjson9e1087fdEncodeTemp(out *jwriter.Writer, in User) {
+        out.RawByte('{')
+        first := true
+        _ = first
+        {
+                const prefix string = ",\"browsers\":"
+                out.RawString(prefix[1:])
+                if in.Browsers == nil && (out.Flags&jwriter.NilSliceAsEmpty) == 0 {
+                        out.RawString("null")
+                } else {
+                        out.RawByte('[')
+                        for v2, v3 := range in.Browsers {
+                                if v2 > 0 {
+                                        out.RawByte(',')
+                                }
+                                out.String(string(v3))
+                        }
+                        out.RawByte(']')
+                }
+        }
+        {
+                const prefix string = ",\"email\":"
+                out.RawString(prefix)
+                out.String(string(in.Email))
+        }
+        {
+                const prefix string = ",\"name\":"
+                out.RawString(prefix)
+                out.String(string(in.Name))
+        }
+        out.RawByte('}')
+}
+
+// MarshalJSON supports json.Marshaler interface
+func (v User) MarshalJSON() ([]byte, error) {
+        w := jwriter.Writer{}
+        easyjson9e1087fdEncodeTemp(&w, v)
+        return w.Buffer.BuildBytes(), w.Error
+}
+
+// MarshalEasyJSON supports easyjson.Marshaler interface
+func (v User) MarshalEasyJSON(w *jwriter.Writer) {
+        easyjson9e1087fdEncodeTemp(w, v)
+}
+
+// UnmarshalJSON supports json.Unmarshaler interface
+func (v *User) UnmarshalJSON(data []byte) error {
+        r := jlexer.Lexer{Data: data}
+        easyjson9e1087fdDecodeTemp(&r, v)
+        return r.Error()
+}
+
+// UnmarshalEasyJSON supports easyjson.Unmarshaler interface
+func (v *User) UnmarshalEasyJSON(l *jlexer.Lexer) {
+        easyjson9e1087fdDecodeTemp(l, v)
+}
+
